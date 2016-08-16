56aebe29718eaa592491dd80dd92d9f84aa6691b
Brian Sayler <sayler.b@gmail.com>
2016-08-15 22:24:48 -0600

    configure webkit as the javascript driver

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -12,8 +12,8 @@ Shoulda::Matchers.configure do |config|
     with.library :rails
   end
 end
-#
-#
+
+
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
