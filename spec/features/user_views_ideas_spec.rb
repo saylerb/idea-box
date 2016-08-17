5b59d2567efcada09283435d7c55f5b22e22baa5
Brian Sayler <sayler.b@gmail.com>
2016-08-16 18:04:43 -0600

    add index feature spec

--- spec/features/user_views_ideas_spec.rb
+++ spec/features/user_views_ideas_spec.rb
@@ -1,16 +1,19 @@
 require "rails_helper"
 
 RSpec.feature "user views ideas", js: true do
+  let!(:idea1) { create(:idea)}
+  let!(:idea2) { create(:idea)}
+
   scenario "that already exist" do
-    idea1 = create(:idea)
-    idea2 = create(:idea)
+
+    idea1_identifier = "#idea-#{idea1.id.to_s}"
+    idea2_identifier = "#idea-#{idea2.id.to_s}"
   
-    visit root_path 
-  
+    visit "/" 
+
     expect(page).to have_current_path(root_path)
 
-    expect(page).to have_content idea1.title
-    expect(page).to have_content idea1.body
-    expect(page).to have_content idea1.quality
+    expect(page).to have_css(idea1_identifier)
+    expect(page).to have_css(idea2_identifier)
   end
 end
