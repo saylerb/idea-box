f79b223b621cda9e73905f8e8f3aa50a0f33f977
Brian Sayler <sayler.b@gmail.com>
2016-08-16 23:17:46 -0600

    implement create functionality
    - add create ajax call
    - remove render json that wasn't working from Ideas controller
    - add form for entering new idea
    - adjust idea request spec not to take params nested under 'idea'

---
+++ app/assets/javascripts/create.js.es6
@@ -0,0 +1,15 @@
+$(document).ready(() => {
+  $("#submitIdea").on('click', e => {
+    e.preventDefault()
+    var newData = $("#newIdea").serialize()
+
+    $.ajax({
+      url: "/api/v1/ideas",
+      type: "POST",
+      dataType: "JSON",
+      data: newData,
+      success: response => appendRow(response),
+      error: error => console.log(error)
+    })
+  })
+})
--- app/controllers/api/v1/ideas_controller.rb
+++ app/controllers/api/v1/ideas_controller.rb
@@ -6,16 +6,16 @@ class Api::V1::IdeasController < Api::V1::ApiBaseController
   def create
     idea = Idea.new(idea_params)
     if idea.save
-      render json: { idea: idea }, status: 201, location: api_v1_ideas_path(idea)
+      render json: idea, status: 201
     else
-      render json: { errors: idea.errors }, status: 422, location: api_v1_ideas_path
+      render json: idea.errors, status: 422
     end
   end
 
   private
 
   def idea_params
-    params.require(:idea).permit(:title, :body, :quality)
+    params.permit(:title, :body, :quality)
   end
 
 end
